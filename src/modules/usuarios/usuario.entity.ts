a3d55c99839448e573e913f89f8b610ba365ad6d
antsrc <as006daw@gmail.com>
2025-06-08 17:33:55 +0200

    Corrección compatibilidad versiones MySQL 2

--- src/modules/usuarios/usuario.entity.ts
+++ src/modules/usuarios/usuario.entity.ts
@@ -23,7 +23,7 @@ export class Usuario {
   @Column({ length: 60 })
   contrasena: string;
 
-  @Column({ type: 'date', name: 'fecha_creacion', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: 'date', name: 'fecha_creacion' })
   fechaCreacion: Date;
 
   @Column({ name: 'usuario_creacion', length: 30, default: 'sistema' })
